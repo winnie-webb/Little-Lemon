7152dcf04ea4af3a207be6cccb91c90a716912ef
winnie-webb <71949190+winnie-webb@users.noreply.github.com>
2024-10-27 21:00:01 -0500

    Added react router and  Main Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+    </>
+  );
 }
 
 export default App;
