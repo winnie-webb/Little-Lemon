4f7dd517b88128acf6d9f0169368f7d9ccae78a3
winnie-webb <71949190+winnie-webb@users.noreply.github.com>
2024-10-27 21:01:07 -0500

    Added Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer>
+      <section>
+        <div>
+          <h3>Document Navigation</h3>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Reservations</li>
+          </ul>
+        </div>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
